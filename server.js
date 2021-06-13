0b0d4c8b0025d9e3716a0eb2e227b2d5c6014c84
nnagm <nnagman@gmail.com>
2021-06-13 13:05:43 +0900

    2.10 authentication part Three

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 import {ApolloServer} from "apollo-server";
 import schema from "./schema";
+import {getUser} from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
     schema,
-    context: {
-        token:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjIzNTU1OTI5fQ.xkCLb66oj4mEz8JWDnO2jgX-G5BfVqduYAZet2PxvGM",
+    context: async ({req}) => {
+        return {
+            loggedInUser: await getUser(req.headers.token),
+        };
     },
 });
 
