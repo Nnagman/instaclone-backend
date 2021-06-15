7c604fe73122372014ef19a9253bcc5cea6bfb8d
nnagm <nnagman@gmail.com>
2021-06-15 16:10:34 +0900

    3.20 S3 Photo Upload part Two

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,8 @@
-import { createWriteStream } from "fs";
+import {createWriteStream} from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import {protectedResolver} from "../users.utils";
+import {uploadPhoto} from "../../shared/shared.utils";
 
 const resolverFn = async (
     _,
@@ -10,14 +11,14 @@ const resolverFn = async (
 ) => {
     let avatarUrl = null;
     if (avatar) {
-        const { filename, createReadStream } = await avatar;
-        const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+        avatarUrl = await uploadPhoto(avatar, loggedInUser.id);
+        /*const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
         const readStream = createReadStream();
         const writeStream = createWriteStream(
             process.cwd() + "/uploads/" + newFilename
         );
         readStream.pipe(writeStream);
-        avatarUrl = `http://localhost:4000/static/${newFilename}`;
+        avatarUrl = `http://localhost:4000/static/${newFilename}`;*/
     }
     let uglyPassword = null;
     if (newPassword) {
@@ -34,7 +35,7 @@ const resolverFn = async (
             email,
             bio,
             ...(uglyPassword && {password: uglyPassword}),
-            ...(avatarUrl && { avatar: avatarUrl }),
+            ...(avatarUrl && {avatar: avatarUrl}),
         },
     });
     if (updatedUser.id) {
