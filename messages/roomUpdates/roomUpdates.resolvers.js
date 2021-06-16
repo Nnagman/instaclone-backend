12c7a244c2cefc6541447e7686124b05b485b73f
nnagm <nnagman@gmail.com>
2021-06-16 15:33:31 +0900

    4.10 Filtering Subscriptions part Two

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -3,10 +3,27 @@ import {NEW_MESSAGE} from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
-    subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({roomUpdates}, {id}) => {
-            return roomUpdates.roomId === id;
-        }
-    ),
+    Subscription: {
+        roomUpdates: {
+            subscribe: async (root, args, context, info) => {
+                const room = await client.room.findUnique({
+                    where: {
+                        id: args.id,
+                    },
+                    select: {
+                        id: true,
+                    },
+                });
+                if (!room) {
+                    throw new Error("You shall not see this.");
+                }
+                return withFilter(
+                    () => pubsub.asyncIterator(NEW_MESSAGE),
+                    ({roomUpdates}, {id}) => {
+                        return roomUpdates.roomId === id;
+                    }
+                )(root, args, context, info);
+            },
+        },
+    },
 };
